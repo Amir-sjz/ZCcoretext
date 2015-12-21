440376c996b0ace4823a32c303aa2c2657f2d411
zhangchengGitName <974897435@qq.com>
2015-12-21 22:54:39 +0800

    Initial Commit

---
+++ ZCcoretext/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCcoretext
+//
+//  Created by 张程 on 15/12/21.
+//  Copyright © 2015年 张程. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZCcoretext/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZCcoretext
+//
+//  Created by 张程 on 15/12/21.
+//  Copyright © 2015年 张程. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
